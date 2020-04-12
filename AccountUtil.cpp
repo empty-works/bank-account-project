7a8ed0ade4cf1b0f8b902ea174be719805a469d1
empty-works <pama.mark@gmail.com>
2020-04-12 03:59:57 +0200

    Added some function prototypes and vector to AccountUtil.

---
+++ AccountUtil.cpp
@@ -0,0 +1,1 @@
+#include "AccountUtil.h"
--- AccountUtil.h
+++ AccountUtil.h
@@ -1,4 +1,14 @@
 #ifndef ACCOUNTUTIL_H_
 #define ACCOUNTUTIL_H_
 
+#include <vector>
+#include "Account.h"
+#include "SavingsAccount.h"
+
+std::vector<Account> account_vec{};
+
+void display_accounts();
+void display_savings_accounts();
+
+
 #endif
