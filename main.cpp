d178d5f12c7cd65e375076efb9132e46b37d0619
empty-works <pama.mark@gmail.com>
2020-04-13 02:00:07 +0200

    Added a couple more tests.

--- main.cpp
+++ main.cpp
@@ -7,10 +7,17 @@
 int main() {
 	
 	std::vector<Account> acc_vec{};
+	
 	acc_vec.push_back(Account {}); // Account name: Unnamed account || Balance: $0.0
        	acc_vec.push_back(Account {"MyAccount"}); // Account name: MyAccount || Balance: $0.0
 	acc_vec.push_back(Account {"MySecondAccount", 1000.00}); // Account name: MySecondAccount || Balance: $1000.00	
 	display(acc_vec);		
+
+	// Deposit amounts
+	deposit(acc_vec, 1000);
+	display(acc_vec);
+
+		
 	
 	return 0;
 }
