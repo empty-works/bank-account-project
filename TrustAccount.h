caff91bcf5c8c4af17937ec9951536de54427d37
empty-works <pama.mark@gmail.com>
2020-04-17 21:17:09 +0200

    Set up class for TrustAccount.

--- TrustAccount.h
+++ TrustAccount.h
@@ -1,6 +1,15 @@
 #ifndef TRUSTACCOUNT_H_
 #define TRUSTACCOUNT_H_
 
+#include <iostream>
+#include "SavingsAccount.h"
 
+class TrustAccount: public SavingsAccount {
+
+	friend std::ostream &operator<<(std::ostream &os, const TrustAccount &acc);
+
+private:
+	
+};
 
 #endif
