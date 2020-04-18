4b2b3957cd58e133d9ca4d9b4874fb1fdce7fb91
empty-works <pama.mark@gmail.com>
2020-04-18 04:09:43 +0200

    Implemented deposit function definition in TrustAccount.

--- TrustAccount.cpp
+++ TrustAccount.cpp
@@ -1,6 +1,11 @@
 #include "TrustAccount.h"
 
-bool TrustAccount::withdraw(const double amount) {
+bool TrustAccount::deposit(const double amount) {
 
-
+	double new_amt = amount;
+	if(amount >= 5000) {
+	
+		new_amt += 50;
+	}	
+	return SavingsAccount::deposit(new_amt);
 }	
